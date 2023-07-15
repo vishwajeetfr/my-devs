6597c7068d30c6998fe04a33fc636bfdfbc670c7
vishwajeet4real <iamvishwajeet1010@gmail.com>
2023-07-15 23:29:42 +0530

    working on Linkedlist

---
+++ LinkedList3/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        LL ob1 = new LL();
+        ob1.InsertAtTail(1);
+        ob1.InsertAtTail(2);
+        ob1.InsertAtTail(3);
+        ob1.insertAt(4,2);
+        ob1.print(ob1.head);
+        ob1.search(4);
+    }
+}
